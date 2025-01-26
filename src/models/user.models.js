859207a4cc7e3ae7779e369cff905de5f7b45be9
priyank gupta <priyankgupta60214@gmail.com>
2025-01-26 22:49:49 +0530

    controlllers

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -38,7 +38,7 @@ const userSchema = new Schema(
                 ref : "Video"
             }
         ],
-        passsword: {
+        password: {
             type: string,
             required: [true, "password is required"]
         },
@@ -51,7 +51,7 @@ const userSchema = new Schema(
 
 userSchema.pre("save", async function (next){
     if(!this.isModified("password"))return next();
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
